d86f2b29040443d4872edfee5b35f97462452658
Jesse Rodriguez <jesse@10-248-70-248.wireless.oregonstate.edu>
2025-08-28 15:34:47 -0700

    Clear up progress bar issues.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ dependencies = [
         'numpy',
         'scipy',
         'matplotlib>=2.2.2',
-        'progressbar2==3.37.1',
+        'progressbar2>=3.60.0',
         'autograd',
         'future'
 ]
